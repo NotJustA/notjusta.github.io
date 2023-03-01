8033998793051797a22b1f2541299959525742cf
Andrew Pavlichuk <73738281+NotJustA@users.noreply.github.com>
2023-03-01 09:46:20 +0200

    Delete squares directory

--- squares/scripts/script.js
+++
@@ -1,7 +0,0 @@
-/* Next item */
-
-const nextItem = i => {
-    let e = $(".main-page > div")[i]
-    console.log(e);
-    timeline.fromTo($("html"), (e.offsetTop / window.scrollY), {scrollTop: window.scrollY}, {scrollTop: e.offsetTop, ease: Power2.easeInOut})
-}
